128a19b6c350dc89afa6e3734e33ed90cc325cf9
Arouna <arouna.sakho@gmail.com>
2017-01-09 14:30:49 +0100

    nombre de secondes dans une année

--- hello.rb
+++ hello.rb
@@ -6,9 +6,4 @@ puts "quel est ton age ?"
 age = gets.chomp
 print "Bienvenue #{nom} tu as #{age}"
 puts "Salut #{nom}" * 3
-$i = 0
-$num = 3
-begin
-    puts "Salut #{nom}"
-    $i += 1
-end while $i < $num
+3.times { puts "Salut #{nom} !"}
---
+++ numbers.rb
@@ -0,0 +1,9 @@
+#calculer le nombres de secondes dans une année
+
+nbj = 365
+nb = 24
+m = 60
+s = 60
+
+resultat = nbj * nb * m * s
+puts resultat
