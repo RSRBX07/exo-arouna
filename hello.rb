f1f06beed5f9a0f73092d7d9f91b8cf1911a1994
Arouna <arouna.sakho@gmail.com>
2017-01-09 12:25:46 +0100

    boucle

--- hello.rb
+++ hello.rb
@@ -1,8 +1,14 @@
 puts "hello "
 puts "Comment t'appeles tu ?"
-nom = gets
+nom = gets.chomp
 puts "Bienvenue "+ nom
 puts "quel est ton age ?"
-age = gets
+age = gets.chomp
 print "Bienvenue #{nom} tu as #{age}"
 puts "Salut #{nom}" * 3
+$i = 0
+$num = 3
+begin
+    puts "Salut #{nom}"
+    $i += 1
+end while $i < $num
