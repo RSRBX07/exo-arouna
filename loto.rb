bb7d50fb33b57c4771a653a9ae94f7599979fc90
Arouna <arouna.sakho@gmail.com>
2017-01-10 15:08:03 +0100

    loto 3eme version

--- loto.rb
+++ loto.rb
@@ -1,6 +1,27 @@
 #Affiche le tirage de 5 nombre entre 1 et 45
 
-nombres = [rand(1..45), rand(1..45).take[0], rand(1..45), rand(1..45), rand(1..45)]
+#nombres = [rand(1..45), rand(1..45), rand(1..45), rand(1..45), rand(1..45)]
 #puts nombres
-ordre = nombres.sort
-puts ordre
+#ordre = nombres.sort
+#puts ordre
+
+
+available_balls = (1..45).to_a
+#shuflle balls and take 5
+picked_balls = available_balls.shuffle.take 5
+
+#puts "le tirage du jour est : #{picked_balls.sort}"
+
+
+#affichage de la cagnotte entre 100 et 500k puts "la cgnotte du jour est de #{}"
+
+vendredi_13 = "n"
+cagnote = 1000000
+cagnoteTreize = 2000000
+
+puts "Somme-nous le vendredi 13 ?(o/n)"
+vendredi_13 = gets
+if vendredi_13 == "o" 
+ cagnote = cagnoteTreize
+end
+puts "le tirage du jour est : #{cagnote}"
