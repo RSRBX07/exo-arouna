f4080252213c0b46d707f12b04ed6f659a28135a
Arouna <arouna.sakho@gmail.com>
2017-01-10 16:29:51 +0100

    work in progress

---
+++ song.rb
@@ -0,0 +1,15 @@
+n=30
+
+puts "Allongeons la jambe"
+puts "
+Ma poule n'a plus qu'#{n-1} poussins
+Ma poule n'a plus qu'#{n-1} poussins
+Elle en avait trente
+Allongeons la jambe
+
+Allongeons la jambe, la jambe
+Car la route est longue !
+Allongeons la jambe, la jambe
+Car la route est longue !
+
+!"
