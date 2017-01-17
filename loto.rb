03dc944b7b043b92a9a56dcdb55f870aa97b1a19
Arouna <arouna.sakho@gmail.com>
2017-01-17 10:29:30 +0100

    rattrapage pierre

--- loto.rb
+++ loto.rb
@@ -1,35 +1,74 @@
-=begin
-#Affiche le tirage de 5 nombre entre 1 et 45
+require 'date'
+class Loto
 
-#nombres = [rand(1..45), rand(1..45), rand(1..45), rand(1..45), rand(1..45)]
-#puts nombres
-#ordre = nombres.sort
-#puts ordre
+  def self.get_grid
+    grid = []
+    5.times do
+      input = gets.to_i
+      grid << input
+    end
+    grid
+  end
 
+  def self.get_flash
+    (1..45).to_a.shuffle.take 5
+  end
 
-available_balls = (1..45).to_a
-#shuflle balls and take 5
-picked_balls = available_balls.shuffle.take 5
+  def has_winner?
+    #comprer tous les bulletins valides avec la grille gagnante
+    sorted_draw = draw.sort
+    @saved_grids.each do |grid|
+      sorted_grid = grid.sort
+      return true if sorted_grid == sorted_draw
+    end
+    return false
+  end
 
-#puts "le tirage du jour est : #{picked_balls.sort}"
+  # enregistre une grille
+  # pour le loto courant
+  def validate_grid grid
+    # @saved_grids ||= []
+    @saved_grids = @saved_grids || []
+    @saved_grids.push grid
+  end 
+  # demander une grille de jeu
 
+  # affichage du montant de la cagnote
+  # entre 100 et 500.000 Euros
+  # le vendredi 13, la cagnote est de 2 millions
+  def vendredi_13?
+    Date.today.day == 13 && Date.today.friday?
+  end
 
-#affichage de la cagnotte entre 100 et 500k puts "la cgnotte du jour est de #{}"
+  
 
-vendredi_13 = "n"
-cagnote = 1000000
-cagnoteTreize = 2000000
+  def prize
+    cagnote = if vendredi_13?
+        2_000_000
+      else
+        100_000
+      end
+    puts "Le montant de la cagnote du jour est de #{cagnote}"
+    cagnote
+  end
 
-puts "Somme-nous le vendredi 13 ?(o/n)"
-vendredi_13 = gets
-if vendredi_13 == "o" 
- cagnote = cagnoteTreize
+  def draw
+    available_balls = (1..45).to_a
+    # shuffle balls and take 5
+    # @picked_balls ||= available_balls.shuffle.take(5)
+    @picked_balls = @picked_balls || available_balls.shuffle.take 5
+
+    puts "Le tirage du jour est : #{@picked_balls.sort}" 
+    @picked_balls
+  end
+
+  def check_grid grid
+    # afficher si gagne ou perdu
+    if grid.sort == draw.sort
+      puts "You win !"
+    else
+      puts "You loose !"
+    end
+  end
+  
 end
-puts "le tirage du jour est : #{cagnote}"
-
-#recuperer les chiffres a l'user et verifier si il a gagner
-=end
-
-puts "Bonjour, entrez vos numero svp :"
-numeroUsers[] = gets
-puts numeroUsers
