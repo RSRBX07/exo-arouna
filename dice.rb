fbeef25df91bf788f9127ff7f27582b4beca595c
Arouna <arouna.sakho@gmail.com>
2017-01-17 10:52:57 +0100

    test

--- dice.rb
+++ dice.rb
@@ -1,6 +1,10 @@
 class Dice
    # roll est une methode d'instance de Dice
   def roll(cheated_value = nil)
-    cheated_value || (1..6).to_a.sample
+    if cheated_value.nil?
+      return (1..6).to_a.sample
+    else
+      return cheated_value
+    end 
   end 
 end
