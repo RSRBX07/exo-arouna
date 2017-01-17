687edf5d466defdbe400fb9a43ab462c4795a7d0
Arouna <arouna.sakho@gmail.com>
2017-01-17 14:24:37 +0100

    max version

--- loto/loto.rb
+++ loto/loto.rb
@@ -1,85 +1,61 @@
-require 'date'
 class Loto
-  attr_reader :picked_balls
+  attr_reader :draw, :players
 
   def initialize
-    @picked_balls = []
+    @players = []
+    @draw =[]
   end
 
-  def self.get_grid
-    grid = []
-    5.times do
-      input = gets.to_i
-      grid.push input
+  def self.create_a_grid
+    result_game = []
+    while result_game.size <= 4
+      print "chiffre #{result_game.size + 1} : "
+      input_number = gets.to_i
+      check_numbers(input_number,result_game)
     end
-    grid
+    result_game
   end
 
-  def self.get_flash
-    (1..45).to_a.shuffle.take 5
-  end
-
-  def has_winner?
-    #comprer tous les bulletins valides avec la grille gagnante
-    sorted_draw = draw.sort
-    @saved_grids.each do |grid|
-      sorted_grid = grid.sort
-      return true if sorted_grid == sorted_draw
+  def self.check_numbers(input_number,result_game)
+    if input_number < 1 || input_number > 45 || result_game.include?(input_number)
+    else
+      result_game.push(input_number)
     end
-    return false
   end
-
-  # enregistre une grille
-  # pour le loto courant
   
-    # verifier que le tirage n'a pas encore eu lieu
-
-  def validate_grid grid
-    if @picked_balls.nil?
-      @saved_grids ||= []
-      @saved_grids.push grid
+  def grid_validation grid
+    if check_draw
+      @players << grid.sort
     else
-      puts "Trop tard" 
-      
-    end
-  end 
-  # demander une grille de jeu
-
-  # affichage du montant de la cagnote
-  # entre 100 et 500.000 Euros
-  # le vendredi 13, la cagnote est de 2 millions
-  def vendredi_13?
-    Date.today.day == 13 && Date.today.friday?
-  end
-
-  def check_grid grid
-    # afficher si gagne ou perdu
-    if grid.sort == draw.sort
-      puts "You win #{prize}!"
-    else
-      puts "You loose !"
+     return false
     end
   end
 
-  def draw
-    available_balls = (1..45).to_a
-    # shuffle balls and take 5
-    # @picked_balls ||= available_balls.shuffle.take(5)
-    @picked_balls = @picked_balls || available_balls.shuffle.take(5)
-
-    puts "Le tirage du jour est : #{@picked_balls.sort}" 
-    @picked_balls
+  def check_result
+    @draw = (1..5).to_a.sample(5).sort
+    last_result = true
+    @players.to_a.each do |grid|
+      if grid == @draw
+        last_result = false
+      end
+      return winner? last_result
+    end 
   end
 
-
+#=====================================
   private
+  #=====================================
 
-  def prize
-    if vendredi_13?
-      2_000_000
+  def check_draw
+    @draw.empty?
+  end
+
+  def winner? last_result
+    if last_result == false
+      return true
     else
-      100_000
+      return false
     end
   end
-  
+
 end
