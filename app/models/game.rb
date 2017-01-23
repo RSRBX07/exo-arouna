68363cfb132fbb8b7a8520b680652c8666619702
Arouna <arouna.sakho@gmail.com>
2017-01-23 15:26:09 +0100

    add and remove player

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,16 @@
 class Game < ApplicationRecord
+
+  def add_player
+    @players += 1
+  end
+  
+  def supp_player
+    @players -= 1
+  end
+  private
+
+  def initialize
+    @players = 0
+  end
+
 end
