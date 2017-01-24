5186395a08249243bdc4716c36c0f21917d41b0c
Arouna <arouna.sakho@gmail.com>
2017-01-24 12:20:21 +0100

    mis show

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,9 @@
 
 Game.destroy_all
 
-Game.create [{name:"Loto du Lundi", description:"le tirage est 12, 42, 52, 68 et 13", players: 123456},
-{name:"Loto du Mardi", description:"le tirage est 1, 42, 52, 68 et 13", players: 654321},
-{name:"Loto du Mercredi", description:"le tirage est 2, 42, 52, 68 et 13", players: 789456},
-{name:"Loto du Jeudi", description:"le tirage est 32, 42, 52, 68 et 13", players: 896547},
-{name:"Loto du Vendredi", description:"le tirage est 22, 42, 52, 68 et 13", players: 453189}]
+Game.create [{name:"Loto du Lundi", description:"Le tirage est 12, 42, 52, 68 et 13", players: 123456},
+{name:"Loto du Mardi", description:"Le tirage est 1, 42, 52, 68 et 13", players: 654321},
+{name:"Loto du Mercredi", description:"Le tirage est 2, 42, 52, 68 et 13", players: 789456},
+{name:"Loto du Jeudi", description:"Le tirage est 32, 42, 52, 68 et 13", players: 896547},
+{name:"Loto du Vendredi", description:"Le tirage est 22, 42, 52, 68 et 13", players: 453189}]
 
